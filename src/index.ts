f3fad9bf232ef83c8a1e4434ea58fc6f22b01a32
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-10-03 11:15:34 +0700

    Update documentation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @packageDocumentation
- * @module Module @frontmeans/dom-events
+ * @module @frontmeans/dom-events
  */
 export * from './dom-event-dispatcher';
 export * from './on-dom-event';
